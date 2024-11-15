8b81f5dcce335d11799ffbc81d83f2a5eed488dd
Finfeny <va0622@edu.turku.fi>
2024-11-15 12:01:29 +0200

    sddsa

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <?php
                     $opiskelijat = $conn->query("SELECT * FROM `opiskelijat`")->fetchAll();
                     foreach ($opiskelijat as $opiskelija) {
-                        echo "<div onClick='toggledisplaySukunimi(this)'>
+                        echo "<div onClick='toggledisplayInfo(this)'>
                         <a class='tunnus'>" . $opiskelija["Opiskelijanumero"] ." ". $opiskelija["Etunimi"] . "</a>" . 
                         "<a class='Info'>" . $opiskelija["Sukunimi"] . "</a></div>";
                     }
@@ -35,7 +35,7 @@
             <?php
                 $opettajat = $conn->query("SELECT * FROM `opettajat`")->fetchAll();
                 foreach ($opettajat as $opettaja) {
-                    echo "<div onClick='toggledisplaySukunimi(this)'>
+                    echo "<div onClick='toggledisplayInfo(this)'>
                     <a class='tunnus'>" . $opettaja["Tunnusnumero"] ." ". $opettaja["Etunimi"] . "</a>" . 
                     "<a class='Info'>" . $opettaja["Sukunimi"] . "</a></div>";
                 }
@@ -51,17 +51,33 @@
                 <?php
                     $kurssit = $conn->query("SELECT * FROM `kurssit`")->fetchAll();
                     foreach ($kurssit as $kurssi) {
-                        echo "<div onClick='toggledisplaySukunimi(this)'>
+                        echo "<div onClick='toggledisplayInfo(this)'>
                         <a class='tunnus'>" . $kurssi["Tunnus"] . " " . $kurssi["Nimi"] . "</a>";
-                        echo "<a class='Info'>" . $kurssi["Kuvaus"] . "<br>" . $kurssi["Alkupäivä"] . " " . $kurssi["Loppupäivä"] . "<br>";
-
+                        
                         $sql = "SELECT * FROM `opettajat` WHERE `Tunnusnumero` = " . $kurssi["Opettaja"];
                         $opettajat = $conn->query($sql)->fetchAll();
-                        echo $opettajat[0]["Etunimi"] . " " . $opettajat[0]["Sukunimi"] . "<br>";
+                        echo "<a class='Info'>" . $opettajat[0]["Etunimi"] . " " . $opettajat[0]["Sukunimi"] . "<br><br>";
+
+                        echo $kurssi["Kuvaus"] . "<br><br>";
 
                         $sql = "SELECT * FROM `tilat` WHERE `Tunnus` = " . $kurssi["Tila"];
                         $tilat = $conn->query($sql)->fetchAll();
-                        echo $tilat[0]["Nimi"]."</a></div>";
+                        echo $tilat[0]["Nimi"] . "<br><br>";
+                        echo $kurssi["Alkupäivä"] . " " . $kurssi["Loppupäivä"];
+
+                        // echo "<button onclick='showChicken(this)'>Näytä ilmoittautumiset</button>" . "</a>";
+                        echo "<button onclick='showChicken(this, \".ilmoittautumiset\")'>Näytä ilmoittautumiset</button>";
+
+                        $sql = "SELECT * FROM `Kurssikirjautumiset` WHERE `Kurssi` = " . $kurssi["Tunnus"];
+                        $ilmoittautumiset = $conn->query($sql)->fetchAll();
+                        echo "<div class='ilmoittautumiset' style='display: none;'>";
+                        echo "<button onclick='hideChicken()'>Piilota ilmoittautumiset</button>";
+                        foreach ($ilmoittautumiset as $ilmoittautuminen) {
+                            $sql = "SELECT * FROM `opiskelijat` WHERE `Opiskelijanumero` = " . $ilmoittautuminen["Opiskelija"];
+                            $opiskelijat = $conn->query($sql)->fetchAll();
+                            echo "<a>Nimi: " . $opiskelijat[0]["Etunimi"] . "<br>Vuosikurssi: " . $opiskelijat[0]["Vuosikurssi"] . "</a>";
+                        }
+                        echo "</div></div>";
                     }
                 ?>
 
@@ -76,7 +92,7 @@
                 <?php
                     $tilat = $conn->query("SELECT * FROM `tilat`")->fetchAll();
                     foreach ($tilat as $tila) {
-                        echo "<div onClick='toggledisplaySukunimi(this)'>
+                        echo "<div onClick='toggledisplayInfo(this)'>
                         <a class='tunnus'>" . $tila["Tunnus"] ." ". $tila["Nimi"] . "</a>" . 
                         "<a class='Info'> Kapasiteetti " . $tila["Kapasiteetti"] . "</a></div>";
                     }
@@ -88,6 +104,37 @@
 
 <script>
 
+    function showChicken(clickedElement, selector) {
+        clickedElement.closest("div").querySelector(selector).style.display = "block";
+    }
+
+    function hideChicken() {
+        document.getElementById("ilmoittautumiset").style.display = "none";
+    }
+
+    // function toggleDisplay(element, selector) {
+    //     var target = element.closest("div").querySelector(selector);
+    //     if (target.style.display === "none" || !target.style.display) {
+    //         target.style.display = "block";
+    //     } else {
+    //         target.style.display = "none";
+    //     }
+    // }
+
+
+    function hideChicken() {
+        document.getElementById("ilmoittautumiset").style.display = "none";
+    }
+    
+    function toggledisplayInfo(clickedElement) {
+        var infoElement = clickedElement.querySelector(".Info");
+        if (infoElement.classList.contains("show")) {
+            infoElement.classList.remove("show");
+        } else {
+            infoElement.classList.add("show");
+        }
+    }
+    
     function toggledisplayOpiskelijat() {
         var element = document.getElementById("opiskelijat");
         if (element.classList.contains("show")) {
@@ -96,15 +143,6 @@
             element.classList.add("show");
         }
     }
-
-    function toggledisplaySukunimi(clickedElement) {
-    var sukunimiElement = clickedElement.querySelector(".Info");
-    if (sukunimiElement.classList.contains("show")) {
-        sukunimiElement.classList.remove("show");
-    } else {
-        sukunimiElement.classList.add("show");
-    }
-}
 
     function toggledisplayOpettajat() {
         var element = document.getElementById("opettajat");
